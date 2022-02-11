2fee161e8b717ae59104e2196dc9011402bfe2df
ocelot8421 <8ocelot@gmail.com>
2022-02-11 22:39:58 +0100

    shopGeneration method v1 is finished

---
+++ src/main/java/hu/progmasters/Franchise.java
@@ -0,0 +1,14 @@
+package main.java.hu.progmasters;
+
+public class Franchise {
+    private String name;
+
+    public Franchise(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
