b693224df482ad5c789c542919e3ad168e76a64f
ocelot8421 <8ocelot@gmail.com>
2022-02-13 16:57:46 +0100

    SQLExeption has been resolved

--- src/main/java/hu/progmasters/BlockGenerator.java
+++ src/main/java/hu/progmasters/BlockGenerator.java
@@ -5,37 +5,37 @@ import java.util.Scanner;
 
 public class BlockGenerator {
 
-//    public static final String DB_URL =
-//            "jdbc:mysql://localhost:3306/blokk_app_local?useSSL=false&serverTimezone=UTC";
-//    public static final String USER = "root";
-//    public static final String PASSWORD = "Test123!";
+    public static final String DB_URL =
+            "jdbc:mysql://blokkapp.cwb4knygtzsz.us-east-1.rds.amazonaws.com:3306/blokk_app?useSSL=false&serverTimezone=UTC";
+    public static final String USER = "root";
+    public static final String PASSWORD = "Test123!";
 
     public static void main(String[] args) {
 
-//        createTable();
+        createTable();
 
 
 
-        System.out.println(Shop.shopGenerator());
+//        System.out.println(Shop.shopGenerator());
     }
 
-//    private static void createTable() {
-//
-//        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)){
-//            Statement statement = connection.createStatement();
-//
-//            String createTable = "CREATE TABLE IF NOT EXISTS kaki" +
-//                    "(ID INT PRIMARY KEY, " +
-//                    "name VARCHAR(50), " +
-//                    "city VARCHAR(50), " +
-//                    "street VARCHAR(50) " +
-//                    "); ";
-//            statement.execute(createTable);
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    private static void createTable() {
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)){
+            Statement statement = connection.createStatement();
+
+            String createTable = "CREATE TABLE IF NOT EXISTS shops" +
+                    "(ID INT PRIMARY KEY, " +
+                    "name VARCHAR(50), " +
+                    "city VARCHAR(50), " +
+                    "street VARCHAR(50) " +
+                    "); ";
+            statement.execute(createTable);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
