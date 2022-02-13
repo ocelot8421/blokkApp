87c615951c721964d23d1fe2c5685167b8eae5c3
ocelot8421 <8ocelot@gmail.com>
2022-02-13 18:50:24 +0100

    BlockGenerator class, createShop method with scanner is done

--- src/main/java/hu/progmasters/BlockGenerator.java
+++ src/main/java/hu/progmasters/BlockGenerator.java
@@ -12,16 +12,42 @@ public class BlockGenerator {
 
     public static void main(String[] args) {
 
-        createTable();
+        createShopTable();
+        createShop();
 
-
-
-//        System.out.println(Shop.shopGenerator());
     }
 
-    private static void createTable() {
+    private static void createShop() {
 
-        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)){
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD); Scanner scanner = new Scanner(System.in)) {
+            Statement statement = connection.createStatement();
+
+            System.out.println("Shop's ID:");
+            int shopID = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Shop's name:");
+            String shopName = scanner.nextLine();
+            System.out.println("City:");
+            String shopCity = scanner.nextLine();
+            System.out.println("Street:");
+            String shopStreet = scanner.nextLine();
+
+            String createShop = "INSERT INTO shops (ID, name, city, street) " +
+                    "VALUES (" + shopID + ", '" + shopName + "', '" + shopCity + "', '" + shopStreet + "')";
+
+            statement.execute(createShop);
+
+            System.out.println("New shop has been created");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void createShopTable() {
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
             Statement statement = connection.createStatement();
 
             String createTable = "CREATE TABLE IF NOT EXISTS shops" +
