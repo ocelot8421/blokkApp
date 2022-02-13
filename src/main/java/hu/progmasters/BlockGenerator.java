ae7b7cf6290075978197c12e65b76d1865a859b0
ocelot8421 <8ocelot@gmail.com>
2022-02-13 16:09:51 +0100

    SQLExeption

--- src/main/java/hu/progmasters/BlockGenerator.java
+++ src/main/java/hu/progmasters/BlockGenerator.java
@@ -5,30 +5,37 @@ import java.util.Scanner;
 
 public class BlockGenerator {
 
-    public static final String DB_URL =
-            "jdbc:mysql://localhost:3306/blokk_app_local?useSSL=false&serverTimezone=UTC";
-    public static final String USER = "root";
-    public static final String PASSWORD = "Test123!";
+//    public static final String DB_URL =
+//            "jdbc:mysql://localhost:3306/blokk_app_local?useSSL=false&serverTimezone=UTC";
+//    public static final String USER = "root";
+//    public static final String PASSWORD = "Test123!";
 
     public static void main(String[] args) {
 
-        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)){
-            Statement statement = connection.createStatement();
+//        createTable();
 
-            String createTable = "CREATE TABLE IF NOT EXISTS shops" +
-                    "(ID INT PRIMARY KEY, " +
-                    "name VARCHAR(50), " +
-                    "city VARCHAR(50), " +
-                    "street VARCHAR(50) " +
-                    "); ";
-            statement.execute(createTable);
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
 
-        // TODO System.out.println(Shop.shopGenerator());
+        System.out.println(Shop.shopGenerator());
     }
+
+//    private static void createTable() {
+//
+//        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)){
+//            Statement statement = connection.createStatement();
+//
+//            String createTable = "CREATE TABLE IF NOT EXISTS kaki" +
+//                    "(ID INT PRIMARY KEY, " +
+//                    "name VARCHAR(50), " +
+//                    "city VARCHAR(50), " +
+//                    "street VARCHAR(50) " +
+//                    "); ";
+//            statement.execute(createTable);
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 
 }
