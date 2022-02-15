acee073f20a50a445e1864f4da7f54549e628dd3
ocelot8421 <8ocelot@gmail.com>
2022-02-15 11:06:17 +0100

    Double DatabaseConfig problem is solved (config packeage has been deleted)

--- src/main/java/hu/progmasters/config/DatabaseConfig.java
+++
@@ -1,8 +0,0 @@
-package hu.progmasters.config;
-
-public class DatabaseConfig {
-    public static final String DB_URL =
-            "jdbc:mysql://blokkapp.cwb4knygtzsz.us-east-1.rds.amazonaws.com:3306/blokk_app?useSSL=false&serverTimezone=UTC&allowMultiQueries=true";
-    public static final String USER = "root";
-    public static final String PASSWORD = "Test123!";
-}
